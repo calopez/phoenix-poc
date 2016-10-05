8aaef54675d6601a7ec20a17e9b25374fc88130f
Carlos Andres Lopez <clopez@ring.com>
2016-10-04 20:54:17 -0500

    Impl Phoenix.Param protocol to suglify the title

--- web/models/video.ex
+++ web/models/video.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -18,6 +19,34 @@ defmodule Rumbl.Video do
     struct
     |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
   end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  @doc """
+  We're implementing the Phoenix.Param protocol for the Rumbl.Video struct.
+  The protocol requires us to implement the to_param function, which receives the video struct itself.
+  We pattern-match on the video slug and ID and use it to build a string as our slug.
+  """
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
+
+
 end
